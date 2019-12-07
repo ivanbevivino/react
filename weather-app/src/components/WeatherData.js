3adbe7042100051260d5ad014e5c6f67ec60125f
ivan bevivino <ivanbevivino@gmail.com>
2019-12-07 16:50:42 -0300

    clase 23

--- weather-app/src/components/WeatherData.js
+++ weather-app/src/components/WeatherData.js
@@ -1,11 +1,15 @@
 import React from "react";
 import WeatherTemperature from "./WeatherTemperature";
 import WeatherExtraInfo from "./WeatherExtraInfo";
+import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from "../constants/weathers";
 
 const WeatherData = () => {
 	return (
 		<div>
-			<WeatherTemperature temperature={25}></WeatherTemperature>
+			<WeatherTemperature
+				temperature={25}
+				wheatherState={SUN}
+			></WeatherTemperature>
 			<WeatherExtraInfo humidity={80} wind={"10 m/s"}></WeatherExtraInfo>
 		</div>
 	);
--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,12 +1,16 @@
 import React from "react";
 import WeatherIcons from "react-weathericons";
-
+import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from "../constants/weathers";
 const WeatherTemperature = props => {
 	const { temperature, wheatherState } = props;
 
 	const icons = {
-		sunny: "day-sunny",
-		fog: "day-fog"
+		[CLOUD]: "cloud",
+		[CLOUDY]: "cloudy",
+		[SUN]: "day-sunny",
+		[RAIN]: "rain",
+		[SNOW]: "snow",
+		[WINDY]: "windy"
 	};
 	const getWeatherIcon = wheatherState => {
 		const icon = icons[wheatherState];
