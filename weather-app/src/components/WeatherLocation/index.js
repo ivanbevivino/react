e486f3939c7f5ba69993ecb9c49f878f4b40f26f
ivan bevivino <ivanbevivino@gmail.com>
2019-12-11 19:50:20 -0300

    clase 38  -  functional to component

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import "./styles.css";
@@ -10,19 +10,22 @@ import {
 	SNOW,
 	WINDY
 } from "../../constants/weathers";
-const WeatherLoaction = () => {
-	const data = {
-		wind: "10 m/s",
-		humidity: 80,
-		temperature: 20,
-		weatherState: SUN
-	};
-	return (
-		<div className="weatherLocationCont">
-			<Location city={"BsAs"}></Location>
-			<WeatherData data={data}></WeatherData>
-		</div>
-	);
+const data = {
+	wind: "10 m/s",
+	humidity: 80,
+	temperature: 20,
+	weatherState: SUN
 };
+
+class WeatherLoaction extends Component {
+	render() {
+		return (
+			<div className="weatherLocationCont">
+				<Location city={"BsAs"}></Location>
+				<WeatherData data={data}></WeatherData>
+			</div>
+		);
+	}
+}
 
 export default WeatherLoaction;
