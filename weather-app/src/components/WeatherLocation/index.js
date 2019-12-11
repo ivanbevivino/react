bcd150f2d95c862933e00a7ad146492ddcf09877
ivan bevivino <ivanbevivino@gmail.com>
2019-12-11 20:27:23 -0300

    clase 40 - this.state ,this.setstate, update

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -16,13 +16,35 @@ const data = {
 	temperature: 20,
 	weatherState: SUN
 };
+const data2 = {
+	wind: "20 m/s",
+	humidity: 90,
+	temperature: 20,
+	weatherState: WINDY
+};
 
 class WeatherLoaction extends Component {
+	constructor() {
+		super();
+		this.state = {
+			city: "Buenos Aires",
+			data: data
+		};
+	}
+	handlerUpdateClick = () => {
+		console.log("actualizado");
+		this.setState({
+			city: "Buenos Aires!!!!",
+			data: data2
+		});
+	};
 	render() {
+		const { city, data } = this.state;
 		return (
 			<div className="weatherLocationCont">
-				<Location city={"BsAs"}></Location>
+				<Location city={city}></Location>
 				<WeatherData data={data}></WeatherData>
+				<button onClick={this.handlerUpdateClick}> actualizar </button>
 			</div>
 		);
 	}
